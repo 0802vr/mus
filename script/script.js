39376244456bd36d00d79cd65d82d15f8eb8faf3
vaso <vasyarovnov0802@gmail.com>
2023-01-22 17:21:06 +0400

    fix10

--- script/script.js
+++ script/script.js
@@ -3,12 +3,18 @@ const splide = new Splide('#partnets_cards', {
       drag   : 'free',
       focus  : 'start',       
       autoWidth: true,
-      /* gap:"20px", */
+      gap:"20px",
       start:0,      
       pagination: false, 
       arrows:false,
       flickPower: number = 10,       
       autoplay: 'pause',
+      breakpoints: {
+        800: {
+          gap:"0",
+        },
+        
+      },
     })
     splide.mount(); 
    
